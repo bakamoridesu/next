06b1848967f49cff69de0be2f8b644b4e95d40b8
Baka Mori <bakamoridesu@gmail.com>
2023-01-13 08:13:40 +0300

    Ecommerce. Added layout

--- ecommerce/components/Layout.tsx
+++ ecommerce/components/Layout.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import Head from 'next/head'
+import React, { ReactNode } from 'react'
+import { Footer } from './Footer'
+import { Navbar } from './Navbar'
 
-export const Layout = () => {
+type Props = {
+  children: ReactNode
+}
+export const Layout = ({ children }: Props) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>Sample store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
