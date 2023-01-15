b7cd7de05829436da93c3d5220f67158f5ac1222
Baka Mori <bakamoridesu@gmail.com>
2023-01-15 06:13:09 +0300

    Ecommerce. Cart page

--- ecommerce/components/Cart.tsx
+++ ecommerce/components/Cart.tsx
@@ -1,7 +1,121 @@
-import React from 'react'
+import {
+  AiOutlineLeft,
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import toast from "react-hot-toast";
+import { useStateContext } from "../context/StateContext";
+import { useRef } from "react";
+import Link from "next/link";
+import product from "../sanity/schemas/product";
+import { urlFor } from "../lib/client";
+import Image from "next/image";
 
 export const Cart = () => {
+  const cartRef = useRef<HTMLDivElement | null>(null);
+  const {
+    totalPrice,
+    totalQuantities,
+    cartItems,
+    setShowCart,
+    toggleCartItemQuantity,
+    removeProduct,
+  } = useStateContext();
+
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item, i) => {
+              const { image, _id, name, price, quantity } = item!;
+              return (
+                <div className="product" key={_id}>
+                  <Image
+                    src={urlFor(image[0]).width(200).url()}
+                    width={200}
+                    height={200}
+                    alt="img"
+                    className="cart-product-image"
+                  />
+                  <div className="item-desc">
+                    <div className="flex top">
+                      <h5>{name}</h5>
+                      <h4>${price}</h4>
+                    </div>
+                    <div className="flex bottom">
+                      <div>
+                        <p className="quantity-desc">
+                          <span
+                            className="minus"
+                            onClick={() => toggleCartItemQuantity(_id, "dec")}
+                          >
+                            <AiOutlineMinus />
+                          </span>
+                          <span className="num">{quantity}</span>
+                          <span
+                            className="plus"
+                            onClick={() => toggleCartItemQuantity(_id, "inc")}
+                          >
+                            <AiOutlinePlus />
+                          </span>
+                        </p>
+                      </div>
+                      <button
+                        type="button"
+                        className="remove-item"
+                        onClick={() => removeProduct(_id)}
+                      >
+                        <TiDeleteOutline />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal</h3>
+              <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick={() => {}}>
+                Pay with Stripe
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
--- ecommerce/context/StateContext.tsx
+++ ecommerce/context/StateContext.tsx
@@ -1,19 +1,23 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 import { toast } from "react-hot-toast";
+import product from "../sanity/schemas/product";
 import { IProduct } from "../types/product";
 
 interface ICartProduct extends IProduct {
-  quantity: number
+  quantity: number;
 }
 type AppContextType = {
   showCart: boolean;
-  cartItems: (ICartProduct | undefined)[] ;
+  cartItems: (ICartProduct | undefined)[];
   totalPrice: number;
   totalQuantities: number;
   quantity: number;
   increaseQuantity: () => void;
   decreaseQuantity: () => void;
+  setShowCart: (condition: boolean) => void;
   onAdd: (product: IProduct, quantity: number) => void;
+  toggleCartItemQuantity: (id: string, value: string) => void;
+  removeProduct: (id: string) => void;
 };
 
 const AppContext = createContext<AppContextType | null>(null);
@@ -24,10 +28,13 @@ type Props = {
 
 export const StateContext = ({ children }: Props) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState<(ICartProduct | undefined)[] >([]);
+  const [cartItems, setCartItems] = useState<(ICartProduct | undefined)[]>([]);
   const [totalPrice, setTotalPrice] = useState<number>(0);
   const [totalQuantities, setTotalQuantities] = useState<number>(0);
   const [quantity, setQuantity] = useState<number>(1);
+
+  let foundProduct;
+  let index;
 
   const increaseQuantity = () => {
     setQuantity((prevQuantity) => prevQuantity + 1);
@@ -43,27 +50,70 @@ export const StateContext = ({ children }: Props) => {
     const checkProductInCart = cartItems?.find(
       (item) => item?._id === product._id
     );
-    
+
     setTotalPrice(
       (prevTotalPrice) => prevTotalPrice + product.price * quantity
     );
-    setTotalQuantities(
-      (prevTotalQuantities) => prevTotalQuantities + quantity
-    );
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
 
     if (checkProductInCart) {
       const updatedCartItems = cartItems?.map((cartProduct) => {
-        if(cartProduct?._id === product._id) return {
-          ...cartProduct,
-          quantity: cartProduct.quantity + quantity
-        }
-      })
+        if (cartProduct?._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
 
       setCartItems(updatedCartItems);
     } else {
-      setCartItems([...cartItems, {...product, quantity}])
+      setCartItems([...cartItems, { ...product, quantity }]);
     }
     toast.success(`${quantity} ${product.name} added to the cart.`);
+  };
+
+  const toggleCartItemQuantity = (id: string, value: string) => {
+    if (value === "inc") {
+      setCartItems(
+        cartItems.map((item) => {
+          if (item?._id === id) {
+            setTotalPrice((totalPrice) => totalPrice + item.price);
+            setTotalQuantities((totalQuantities) => totalQuantities + 1);
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          }
+          return item;
+        })
+      );
+    } else if (value === "dec") {
+      const foundProduct = cartItems.find((item) => item?._id === id) as ICartProduct
+      if(foundProduct.quantity === 1) {
+        removeProduct(id)
+      } else {
+        setCartItems(
+          cartItems.map((item) => {
+            if (item?._id === id) {
+              setTotalPrice((totalPrice) => totalPrice - foundProduct.price);
+              setTotalQuantities((totalQuantities) => totalQuantities - 1);
+              return {
+                ...item,
+                quantity: item.quantity - 1 > 0 ? item.quantity - 1 : 1,
+              };
+            }
+            return item;
+          })
+        );
+      }
+    }
+  };
+
+  const removeProduct = (id: string) => {
+    const foundProduct = cartItems.find((item) => item?._id === id) as ICartProduct
+    setCartItems(cartItems.filter((item) => item?._id !== id));
+    setTotalPrice((totalPrice) => totalPrice - foundProduct.price * foundProduct.quantity);
+    setTotalQuantities((totalQuantities) => totalQuantities - foundProduct.quantity);
   };
 
   return (
@@ -77,6 +127,9 @@ export const StateContext = ({ children }: Props) => {
         increaseQuantity,
         decreaseQuantity,
         onAdd,
+        setShowCart,
+        toggleCartItemQuantity,
+        removeProduct,
       }}
     >
       {children}
@@ -87,11 +140,11 @@ export const StateContext = ({ children }: Props) => {
 export const useStateContext = () => {
   const stateContext = useContext(AppContext);
 
-  if(!stateContext) {
+  if (!stateContext) {
     throw new Error(
       "useStateContext has to be used within <AppContext.Provider>"
-    )
+    );
   }
 
-  return stateContext
+  return stateContext;
 };
